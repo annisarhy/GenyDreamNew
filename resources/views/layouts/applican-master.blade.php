873906f985077ad9c72c3fe0a8b016d51f9b9725
Fajar Faturrahman <52985772+FajarFaturrahman@users.noreply.github.com>
2020-07-26 10:17:16 +0700

    add master

--- resources/views/layouts/applican-master.blade.php
+++ resources/views/layouts/applican-master.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title></title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,7 +21,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/.css') }}" rel="stylesheet">
 </head>
 
 <body>
