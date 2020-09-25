334b0adc53089d95ba3eaa169e7790cc276e1784
Fajar Faturrahman <52985772+FajarFaturrahman@users.noreply.github.com>
2020-09-25 09:40:10 +0700

    revisi sidebar

--- resources/views/admin/sidebar-admin-master/sidebar-admin-master.blade.php
+++ resources/views/admin/sidebar-admin-master/sidebar-admin-master.blade.php
@@ -6,11 +6,12 @@
 <!-- Styles -->
     <link href="{{ asset('css/admin/dashboard-sidebar-master.css') }}" rel="stylesheet">   
 
-    <div class="d-flex mt-5 mb-5" id="wrapper">
+    <div class="mt-5 mb-5" id="wrapper">
 
         <!-- sidebar -->
-        <div class=" border-right" id="sidebar-wrapper">            
+        <div class="border-right" id="sidebar-wrapper">            
             <div class="list-group list-group-flush">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
                 <a href="{{ route('admin.beranda') }}" class="list-group-item list-group-item-action {{ request()->route()->named('admin.beranda') ? 'active' : '' }}"><img src="{{ asset('images/gg_menu-grid-gray.png') }}" alt="" class="img-fluid mr-2">DASHBOARD</a>
                 <a href="{{ route('admin.perusahaan.index') }}" class="list-group-item list-group-item-action {{ request()->route()->named('admin.perusahaan.index') ? 'active' : '' }}"><img src="{{ asset('images/bi_building-gray.png') }}" alt="" class="img-fluid mr-2">KELOLA PERUSAHAAN</a>
                 <a href="{{ route('admin.user.index') }}" class="list-group-item list-group-item-action {{ request()->route()->named('admin.user.index') ? 'active' : '' }}"><img src="{{ asset('images/ant-design_user-outlined-gray.png') }}" alt="" class="img-fluid mr-2">KELOLA USER</a>
@@ -25,10 +26,9 @@
         <div id="page-content-wrapper">
             <!-- content -->
             <div class="container-fluid">
-                <button class="btn btn-primary mb-3" id="menu-toggle">Sidebar</button>
-                <div class="row">
-                    @yield('content-dashboard')
-                </div>
+                <button class="btn btn-primary mb-3" onclick="openNav()">Open Sidebar</button>
+                
+                    @yield('content-dashboard')                
                 
             </div>
         </div>
@@ -39,5 +39,15 @@
 @yield('js-dashboard')
 
 @section('js')
+<script>
+    function openNav() {
+    document.getElementById("sidebar-wrapper").style.width = "250px";
+    document.getElementById("page-content-wrapper").style.marginLeft = "250px";
+    }
 
+    function closeNav() {
+    document.getElementById("sidebar-wrapper").style.width = "0";
+    document.getElementById("page-content-wrapper").style.marginLeft= "0";
+    }
+</script>
 @endsection
