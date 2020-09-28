d10e8e24a7a633e480e4f2a9f93c1fbac05c6fc5
Fajar Faturrahman <52985772+FajarFaturrahman@users.noreply.github.com>
2020-09-28 22:39:21 +0700

    tambahan profil

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -11,9 +11,9 @@
     </div>    
 
     
-
+    <!-- jumlah data -->
     <div class="row mt-5 pl-1 pr-2">
-        <div class="col-md-3 mt-2">
+        <div class="col-md-3 mt-2" onclick="window.location='{{ route('admin.perusahaan.index') }}'">
             <div class="card-info-header box-shadow rounded pl-1 pt-2 pb-2">
                 <div class="d-flex">                                
                     <div class="img-left-company col-md-3">
@@ -28,7 +28,7 @@
             </div>            
         </div>
 
-        <div class="col-md-3 mt-2">
+        <div class="col-md-3 mt-2" onclick="window.location='{{ route('admin.user.index') }}'">
             <div class="card-info-header box-shadow rounded pl-1 pt-2 pb-2">
                 <div class="d-flex">                                
                     <div class="img-left-applicant col-md-3">
@@ -36,38 +36,38 @@
                     </div>
                 
                     <div class="col-md-9">
-                        <p class="info-header mb-0">Jumlah Pelamar</p>
+                        <p class="info-header mb-0">Jumlah User</p>
                         <p class="info-applicant-total mb-0">992</p>
                     </div>
                 </div>
             </div>            
         </div>
 
-        <div class="col-md-3 mt-2">
+        <div class="col-md-3 mt-2" onclick="window.location='{{ route('admin.berita.index') }}'">
             <div class="card-info-header box-shadow rounded pl-1 pt-2 pb-2">
                 <div class="d-flex">                                
-                    <div class="img-left-accepted col-md-3">
-                        <img src="{{ asset('images/checklist-green.png') }}" class="img-fluid img-left" alt="">
+                    <div class="img-left-content col-md-3">
+                        <img src="{{ asset('images/bx_bx-news.png') }}" class="img-fluid img-left" alt="">
                     </div>
                 
                     <div class="col-md-9">
-                        <p class="info-header mb-0">Jumlah Diterima</p>
-                        <p class="info-accepted-total mb-0">992</p>
+                        <p class="info-header mb-0">Jumlah Konten</p>
+                        <p class="info-content-total mb-0">992</p>
                     </div>
                 </div>
             </div>            
         </div>
 
-        <div class="col-md-3 mt-2">
-            <div class="card-info-header box-shadow rounded pl-1 pt-2 pb-2">
+        <div class="col-md-3 mt-2" >
+            <div class="card-info-header box-shadow rounded pl-1 pt-2 pb-2" onclick="window.location='{{ route('admin.verifikasi.index') }}'">
                 <div class="d-flex">                                
-                    <div class="img-left-rejected col-md-3">
-                        <img src="{{ asset('images/eva_close-fill-red.png') }}" class="img-fluid img-left" alt="">
+                    <div class="img-left-premium col-md-3">
+                        <img src="{{ asset('images/la_money-bill-solid-gray.png') }}" class="img-fluid img-left" alt="">
                     </div>
                 
                     <div class="col-md-9">
-                        <p class="info-header mb-0">Jumlah Ditolak</p>
-                        <p class="info-rejected-total mb-0">992</p>
+                        <p class="info-header mb-0">Jumlah Premium</p>
+                        <p class="info-premium-total mb-0">992</p>
                     </div>
                 </div>
             </div>            
@@ -86,69 +86,77 @@
 
                 <div class="row">
                     <div class="col-md-6 mt-2">                        
-                        <div class="menu-list pl-1 pt-2 pb-2 pr-4" onclick="window.location='{{ route('admin.perusahaan.index') }}'">
+                        <div class="menu-list pl-1 " onclick="window.location='{{ route('admin.perusahaan.index') }}'">
                             <div class="d-flex">                                
-                                <div class="col-md-3 menu-list-img">
+                                <div class="col-md-3 pt-2 pb-2 menu-list-img">
                                     <img src="{{ asset('images/bi_building-blue.png') }}" class="img-fluid img-left-menu" alt="">
                                 </div>
                             
-                                <div class="col-md-9">
+                                <div class="col-md-8 pt-3 pb-2">
                                     <p class="menu-header font-weight-bold mb-0">Kelola Perusahaan</p>
                                     <p class="mb-0">Mulai Sekarang</p>                                    
                                 </div>
 
-                                <i class="fa fa-angle-right mt-2" aria-hidden="true"></i>
+                                <div class="row-option text-center">
+                                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                </div>                                
                             </div>
                         </div>                             
                     </div>                
 
                     <div class="col-md-6 mt-2">
-                        <div class="menu-list pl-1 pt-2 pb-2 pr-4" onclick="window.location='{{ route('admin.verifikasi.index') }}'">
+                        <div class="menu-list pl-1" onclick="window.location='{{ route('admin.verifikasi.index') }}'">
                             <div class="d-flex">                                
-                                <div class="col-md-3 menu-list-img" href="#">
+                                <div class="col-md-3 menu-list-img pt-2 pb-2" href="#">
                                     <img src="{{ asset('images/la_money-bill-solid-blue.png') }}" class="img-fluid img-left-menu" alt="">
                                 </div>
                             
-                                <div class="col-md-9">
+                                <div class="col-md-8 pt-3 pb-2">
                                     <p class="menu-header font-weight-bold mb-0">Verikasi Pembayaran</p>
                                     <p class="mb-0">Mulai Sekarang</p>                                    
                                 </div>
 
-                                <i class="fa fa-angle-right mt-2" aria-hidden="true"></i>
+                                <div class="row-option text-center">
+                                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                </div>                                
                             </div>
                         </div>            
                     </div>                
 
                     <div class="col-md-6 mt-2">
-                        <div class="menu-list pl-1 pt-2 pb-2 pr-4" onclick="window.location='{{ route('admin.user.index') }}'">
+                        <div class="menu-list pl-1" onclick="window.location='{{ route('admin.user.index') }}'">
                             <div class="d-flex">                                
-                                <div class="col-md-3 menu-list-img">
+                                <div class="col-md-3 menu-list-img pt-2 pb-2">
                                     <img src="{{ asset('images/ant-design_user-outlined-blue.png') }}" class="img-fluid img-left-menu" alt="">
                                 </div>
                             
-                                <div class="col-md-9">
+                                <div class="col-md-8 pt-3 pb-2">
                                     <p class="menu-header font-weight-bold mb-0">Kelola User</p>
                                     <p class="mb-0">Mulai Sekarang</p>                                    
                                 </div>
 
-                                <i class="fa fa-angle-right mt-2" aria-hidden="true"></i>
+                                <div class="row-option text-center">
+                                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                </div>                                
                             </div>
                         </div>            
                     </div>                
 
                     <div class="col-md-6 mt-2">
-                        <div class="menu-list pl-1 pt-2 pb-2 pr-4" onclick="window.location='{{ route('admin.berita.index') }}'">
+                        <div class="menu-list pl-1" onclick="window.location='{{ route('admin.berita.index') }}'">
                             <div class="d-flex">                                
-                                <div class="col-md-3 menu-list-img" href="#">
+                                <div class="col-md-3 pt-2 pb-2 menu-list-img" href="#">
                                     <img src="{{ asset('images/bx_bx-news-blue.png') }}" class="img-fluid img-left-menu" alt="">
                                 </div>
                             
-                                <div class="col-md-9">
+                                <div class="col-md-8 pt-3 pb-2">
                                     <p class="menu-header font-weight-bold mb-0">Kelola Konten</p>
                                     <p class="mb-0">Mulai Sekarang</p>                                    
                                 </div>
 
-                                <i class="fa fa-angle-right mt-2" aria-hidden="true"></i>
+                                <div class="row-option text-center">
+                                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                </div>                                
                             </div>
                         </div>            
                     </div>                
